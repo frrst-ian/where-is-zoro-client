779c5f28fe6972a84df142ff76190319cada3944
frrst-ian <frrstxd@proton.me>
2025-10-05 11:40:28 +0800

    feat: protect routes

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -2,13 +2,23 @@ import { Routes, Route } from "react-router-dom";
 import Game from "./components/containers/GamePageContainer";
 import Auth from "./components/containers/AuthContainer";
 import Signup from "./components/containers/SignupContainer";
+import ProtectedRoute from "./components/containers/ProtectedRoute";
+import NotFound from "./components/ui/NotFound";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/game" element={<Game />} />
+      <Route
+        path="/game"
+        element={
+          <ProtectedRoute>
+            <Game />
+          </ProtectedRoute>
+        }
+      />
       <Route path="/login" element={<Auth />} />
       <Route path="/signup" element={<Signup />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
