f3a59865a02564a3f2a5cbe7942942a84773c9d9
frrst-ian <frrstxd@proton.me>
2025-10-05 12:20:04 +0800

    fix: remove trailing semi colons and commas

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
-import {UserProvider;} from "../src/context/UserContext";
+import {UserProvider} from "../src/context/UserContext";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
@@ -9,5 +9,5 @@ createRoot(document.getElementById("root")).render(
     <UserProvider>
       <App />
     </UserProvider>
-  </StrictMode>,
+  </StrictMode>
 );
